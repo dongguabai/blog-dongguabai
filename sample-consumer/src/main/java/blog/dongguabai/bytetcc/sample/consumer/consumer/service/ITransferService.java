8742fe3b01510ecd70a0f69136ec51d26bf9bd49
Dongguabai <824555252@qq.com>
2021-09-26 00:59:08 +0800

    chore:move

--- sample-consumer/src/main/java/blog/dongguabai/bytetcc/sample/consumer/consumer/service/ITransferService.java
+++
@@ -1,7 +0,0 @@
-package blog.dongguabai.bytetcc.sample.consumer.consumer.service;
-
-public interface ITransferService {
-
-	public void transfer(String sourceAcctId, String targetAcctId, double amount);
-
-}
--- sample-provider/src/main/java/blog/dongguabai/bytetcc/sample/provider/service/IAccountService.java
+++
@@ -1,9 +0,0 @@
-package blog.dongguabai.bytetcc.sample.provider.service;
-
-public interface IAccountService {
-
-	public void increaseAmount(String accountId, double amount);
-
-	public void decreaseAmount(String accountId, double amount);
-
-}
