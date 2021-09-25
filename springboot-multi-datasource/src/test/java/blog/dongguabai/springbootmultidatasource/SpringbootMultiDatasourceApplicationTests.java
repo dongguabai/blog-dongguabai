1fde4824b8bc91ba7f09ef1c7f8cfdc304c8e8cf
Dongguabai <824555252@qq.com>
2021-09-26 00:19:16 +0800

    chore:init

---
+++ springboot-multi-datasource/src/test/java/blog/dongguabai/springbootmultidatasource/SpringbootMultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.dongguabai.springbootmultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultiDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
