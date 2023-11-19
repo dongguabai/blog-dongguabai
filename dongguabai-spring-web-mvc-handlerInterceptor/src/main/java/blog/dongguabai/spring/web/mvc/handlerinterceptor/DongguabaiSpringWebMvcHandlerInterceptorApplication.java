c4550a8fc3f549158ec5b350278b3fd62e940ce0
Dongguabai <824555252@qq.com>
2023-11-19 22:58:20 +0800

    chore:init

---
+++ dongguabai-spring-web-mvc-handlerInterceptor/src/main/java/blog/dongguabai/spring/web/mvc/handlerinterceptor/DongguabaiSpringWebMvcHandlerInterceptorApplication.java
@@ -0,0 +1,13 @@
+package blog.dongguabai.spring.web.mvc.handlerinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongguabaiSpringWebMvcHandlerInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongguabaiSpringWebMvcHandlerInterceptorApplication.class, args);
+	}
+
+}
---
+++ dongguabai-spring-web-mvc-handlerInterceptor/src/test/java/blog/dongguabai/spring/web/mvc/handlerinterceptor/DongguabaiSpringWebMvcHandlerInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.dongguabai.spring.web.mvc.handlerinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongguabaiSpringWebMvcHandlerInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
