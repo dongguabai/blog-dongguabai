a5fe3817091a1d1219bdc3ee14b46cbb005f3863
Dongguabai <824555252@qq.com>
2024-01-03 19:15:54 +0800

    feat:拖拽一个 Java 文件，执行 main 函数

---
+++ others/src/main/java/blog/dongguabai/others/drag_run/test/SimpleTest.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * @author dongguabai
+ * @date 2024-01-03 17:02
+ */
+public class SimpleTest {
+
+    public static void main(String[] args) {
+        System.out.println(new Date().toLocaleString());
+    }
+}
---
+++ others/src/main/java/blog/dongguabai/others/drag_run/test/SimpleTest2.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+/**
+ * @author dongguabai
+ * @date 2024-01-03 17:02
+ */
+public class SimpleTest2 {
+
+    public static void main(String[] args) {
+        System.out.println(new Date().toLocaleString());
+    }
+}
