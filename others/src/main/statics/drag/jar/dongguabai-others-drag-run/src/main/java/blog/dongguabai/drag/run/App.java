e720e734141b323e35ca9a8a96785dd485384987
Dongguabai <824555252@qq.com>
2024-01-04 14:17:31 +0800

    feat:添加jars

---
+++ others/src/main/statics/drag/jar/dongguabai-others-drag-run/src/main/java/blog/dongguabai/drag/run/App.java
@@ -0,0 +1,14 @@
+package blog.dongguabai.drag.run;
+
+import com.google.common.collect.Lists;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList(new Date().toLocaleString()));
+    }
+}
---
+++ others/src/main/statics/drag/jar/dongguabai-others-drag-run/src/main/java/blog/dongguabai/others/drag/run/DragRunDemo2.java
@@ -0,0 +1,16 @@
+package blog.dongguabai.others.drag.run;
+
+import com.google.common.collect.Lists;
+
+import java.util.Date;
+
+/**
+ * @author dongguabai
+ * @date 2024-01-04 10:23
+ */
+public class DragRunDemo2 {
+
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList(new Date().toLocaleString()));
+    }
+}
