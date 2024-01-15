16f5e599a0d88ad3bb79aa9149c8a344e17b0a98
Dongguabai <824555252@qq.com>
2024-01-15 12:17:21 +0800

    feat:agent 重构

--- dongguabai-arthas/dongguabai-arthas-agent/src/main/java/blog/dongguabai/arthas/Agent.java
+++ dongguabai-arthas/dongguabai-arthas-agent/src/main/java/blog/dongguabai/arthas/Agent.java
@@ -5,71 +5,67 @@ import javassist.CtClass;
 import javassist.CtMethod;
 
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
-import java.security.ProtectionDomain;
+import java.lang.instrument.UnmodifiableClassException;
+import java.util.Arrays;
 
 public class Agent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("Agent premain start");
-        try {
-            initializeAgent(agentArgs, inst);
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-        System.out.println("Agent premain end");
+        executeAgent("premain", agentArgs, inst);
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        System.out.println("Agent agentmain start");
+        executeAgent("agentmain", agentArgs, inst);
+    }
+
+    private static void executeAgent(String agentType, String agentArgs, Instrumentation inst) {
+        System.out.println("Agent " + agentType + " start");
         try {
             initializeAgent(agentArgs, inst);
         } catch (Throwable e) {
             e.printStackTrace();
         }
-        System.out.println("Agent agentmain end");
+        System.out.println("Agent " + agentType + " end");
     }
 
     private static void initializeAgent(String agentArgs, final Instrumentation inst) throws Exception {
         String[] args = agentArgs.split("#");
         final String className = args[0];
         final String methodName = args[1];
-        System.out.println("className:" + className);
-        System.out.println("methodName:" + methodName);
-        Class[] allLoadedClasses = inst.getAllLoadedClasses();
-        System.out.println(allLoadedClasses.length);
 
-        inst.addTransformer(new ClassFileTransformer() {
-            @Override
-            public byte[] transform(ClassLoader loader, String className1, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-                if (!className1.replace("/", ".").equals(className)) {
-                    return null;
-                }
+        inst.addTransformer(createTransformer(className, methodName), true);
 
-                try {
-                    ClassPool cp = ClassPool.getDefault();
-                    CtClass cc = cp.get(className1.replace("/", "."));
-                    CtMethod m = cc.getDeclaredMethod(methodName);
-                    System.out.println("Transforming class: " + cc.getName());
-                    System.out.println("Transforming method: " + m.getName());
-                    m.insertBefore("System.out.println(\"Arguments: \" + java.util.Arrays.toString($args));");
-                    m.insertAfter("System.out.println(\"Return: \" + $_);");
-                    return cc.toBytecode();
-                } catch (Exception e) {
-                    System.out.println("Failed to transform class: " + className1);
-                    e.printStackTrace();
-                    return null;
-                }
+        Arrays.stream(inst.getAllLoadedClasses())
+                .filter(clazz -> clazz.getName().equals(className))
+                .findFirst()
+                .ifPresent(clazz -> {
+                    try {
+                        inst.retransformClasses(clazz);
+                    } catch (UnmodifiableClassException e) {
+                        System.out.println("Failed to retransform class: " + clazz.getName());
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    private static ClassFileTransformer createTransformer(String className, String methodName) {
+        return (loader, className1, classBeingRedefined, protectionDomain, classfileBuffer) -> {
+            if (!className1.replace("/", ".").equals(className)) {
+                return null;
             }
-        }, true);
-
-        for (Class<?> clazz : allLoadedClasses) {
-            System.out.println("Loaded class: " + clazz.getName());
-            if (clazz.getName().equals(className)) {
-                System.out.println("Retransforming class: " + clazz.getName());
-                inst.retransformClasses(clazz);
+            try {
+                ClassPool cp = ClassPool.getDefault();
+                CtClass cc = cp.get(className1.replace("/", "."));
+                CtMethod m = cc.getDeclaredMethod(methodName);
+                m.insertBefore("System.out.println(\"Arguments: \" + java.util.Arrays.toString($args));");
+                m.insertAfter("System.out.println(\"Return: \" + $_);");
+                return cc.toBytecode();
+            } catch (Exception e) {
+                System.out.println("Failed to transform class: " + className1);
+                e.printStackTrace();
+                return null;
             }
-        }
+        };
     }
 }
