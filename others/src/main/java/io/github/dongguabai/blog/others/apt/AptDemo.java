75b7dbb0e157d02ea20cc1014b402270aac3d7fc
dongguabai <guabaidong@gmail.com>
2024-03-28 14:44:26 +0800

    apt

--- others/src/main/java/io/github/dongguabai/blog/others/apt/AptDemo.java
+++ others/src/main/java/io/github/dongguabai/blog/others/apt/AptDemo.java
@@ -4,5 +4,9 @@ package io.github.dongguabai.blog.others.apt;
  * @author dongguabai
  * @date 2024-03-27 21:23
  */
+@GenerateGettersSetters
 public class AptDemo {
+
+    private String username;
+
 }
